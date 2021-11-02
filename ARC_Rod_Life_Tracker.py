999896e02cd9e8ce5a4273f22a80912d89ce69c7
Dylan Bowersock <dbowersock18@gmail.com>
2021-11-02 11:05:42 -0600

    Project Setup

--- ARC_Rod_Life_Tracker.py
+++ ARC_Rod_Life_Tracker.py
@@ -1,9 +1,23 @@
-import sys
+#! Will import as needed to understand what each import does
+
+
+#! OVERVIEW
+#! Create an Array where each element represents [1] rod string. Array will be 262 elements long to represent number of rods in hole
+#! Data in this well will be the best
+#! A seperate array in a similiar format will capture all the data that was processed and used in the evaluation!
+
 
 def main():
     print ("test")
 
+    #! Retreive program data from JSON file
 
+    #! Read Excel File to import new information into program data
+    #! Convert Excel file to DataTable
+
+    #! Process data from DataTable
+
+    #! Store program data in JSON File so program 
 
 if __name__=="__main__":
     main()
